338087a57b5a67a4ff8f7e3ac1501a39387066ec
yourgotocoder <yourgotocoder@gmail.com>
2022-04-18 11:10:21 +0530

    Create a Todo component

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,13 +1,10 @@
+import Todo from "./Todo";
+
 function App() {
     return (
         <div>
             <h1>My Todos</h1>
-            <div className="card">
-                <h2>Title</h2>
-                <div className="actions">
-                    <button className="btn">Delete</button>
-                </div>
-            </div>
+            <Todo />
         </div>
     );
 }
