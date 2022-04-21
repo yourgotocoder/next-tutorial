0a443057ef10f1da627ab3247a0aac3c4867d878
yourgotocoder <yourgotocoder@gmail.com>
2022-04-21 15:12:28 +0530

    Section 4 completed

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/UI/error-alert";
 
 const EventDetailPage = () => {
     const router = useRouter();
@@ -11,7 +12,7 @@ const EventDetailPage = () => {
     const event = getEventById(eventId);
 
     if (!event) {
-        return <p>No event found</p>;
+        return <ErrorAlert>No event found</ErrorAlert>;
     }
 
     return (
