d086fe1394ea9ee8554dba5632daaf19b1b00ee5
yourgotocoder <yourgotocoder@gmail.com>
2022-04-22 10:14:06 +0530

    Added getStaticProps plus used the fs and path within  a next page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 
-
-const HomePage = () => {
-    
+const HomePage = (props) => {
+    const { products } = props;
 
     return (
-        <div>
-           Home Page
-        </div>
+        <ul>
+            {products.map((product) => (
+                <li key={product.id}>{product.title}</li>
+            ))}
+        </ul>
     );
 };
+
+export async function getStaticProps() {
+    const fs = await import("fs/promises");
+    const path = await import("path");
+
+    const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
+
+    const jsonData = await fs.readFile(filepath);
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            products: data.products,
+        },
+    };
+}
 
 export default HomePage;
