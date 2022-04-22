e2dc4853a7ecc6fe19b446e988444294d57a1ccf
yourgotocoder <yourgotocoder@gmail.com>
2022-04-22 16:47:04 +0530

    Added getStaticProps to index page for better optimisation

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
 import React from "react";
 import EventList from "../components/events/EventList";
-import { getFeaturedEvents } from "../dummy-data";
 
-const HomePage = () => {
-    const featuredEvents = getFeaturedEvents();
-
-    return (
-        <div>
-            <EventList items={featuredEvents} />
-        </div>
-    );
+const HomePage = (props) => {
+    console.log(props);
+    if (!props.featuredEvents) {
+        return <p>Loading</p>;
+    }
+    if (props.featuredEvents) {
+        return (
+            <div>
+                <EventList items={props.featuredEvents} />
+            </div>
+        );
+    }
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+    const response = await fetch(
+        "https://next-js-cou-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+    );
+    const data = await response.json();
+    const transformedData = [];
+
+    for (let key in data) {
+        transformedData.push({
+            id: key,
+            title: data[key].title,
+            description: data[key].description,
+            location: data[key].location,
+            date: data[key].date,
+            image: data[key].image,
+            isFeatured: data[key].image,
+        });
+    }
+
+    return {
+        props: {
+            featuredEvents: transformedData,
+        },
+    };
+}
