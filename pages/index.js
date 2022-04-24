971a6c7e54be4dd6b34d686e7759e84e7e3aef22
yourgotocoder <yourgotocoder@gmail.com>
2022-04-24 14:21:39 +0530

    Section 8 clean state

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
-
-function HomePage(props) {
+function Homepage() {
   return (
-    <Fragment>
-      <Head>
-        <title>Max' Blog</title>
-        <meta
-          name='description'
-          content='I post about programming and web development.'
-        />
-      </Head>
-      <Hero />
-      <FeaturedPosts posts={props.posts} />
-    </Fragment>
+    <div>
+      <h1>Homepage</h1>
+    </div>
   );
 }
 
-export function getStaticProps() {
-  const featuredPosts = getFeaturedPosts();
-
-  return {
-    props: {
-      posts: featuredPosts,
-    },
-  };
-}
-
-export default HomePage;
+export default Homepage;
