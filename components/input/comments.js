3499948ce626652d2ef51d0f452d387f037d8bce
yourgotocoder <yourgotocoder@gmail.com>
2022-04-25 10:05:32 +0530

    Connected comments api to frontend

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,31 +1,39 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import CommentList from './comment-list';
-import NewComment from './new-comment';
-import classes from './comments.module.css';
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
 
 function Comments(props) {
-  const { eventId } = props;
+    const { eventId } = props;
 
-  const [showComments, setShowComments] = useState(false);
+    const [showComments, setShowComments] = useState(false);
 
-  function toggleCommentsHandler() {
-    setShowComments((prevStatus) => !prevStatus);
-  }
+    function toggleCommentsHandler() {
+        setShowComments((prevStatus) => !prevStatus);
+    }
 
-  function addCommentHandler(commentData) {
-    // send data to API
-  }
+    function addCommentHandler(commentData) {
+        fetch("/api/comments/" + props.eventId, {
+            method: "POST",
+            body: JSON.stringify(commentData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
 
-  return (
-    <section className={classes.comments}>
-      <button onClick={toggleCommentsHandler}>
-        {showComments ? 'Hide' : 'Show'} Comments
-      </button>
-      {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
-    </section>
-  );
+    return (
+        <section className={classes.comments}>
+            <button onClick={toggleCommentsHandler}>
+                {showComments ? "Hide" : "Show"} Comments
+            </button>
+            {showComments && <NewComment onAddComment={addCommentHandler} />}
+            {showComments && <CommentList />}
+        </section>
+    );
 }
 
 export default Comments;
